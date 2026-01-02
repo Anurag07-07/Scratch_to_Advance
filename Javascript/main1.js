c1482af1dc9d89b07ba5590a873540dd9be089a1
Anurag07-07 <adityanv8@gmail.com>
2026-01-03 01:19:13 +0530

    completed JS

--- Javascript/main1.js
+++ Javascript/main1.js
@@ -129,115 +129,214 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////DAY2//////////////////////////////////////////////////////////////////////
 
-let obj = {
-  name:"Anurag"
+// let obj = {
+//   name:"Anurag"
+// }
+
+// console.log(obj.toString());
+// console.log(obj.__proto__);
+
+
+// //Custom Prototype
+// let parent = {
+//   greet:function(){
+//     console.log("Hello");
+//   }
+// } 
+
+// let student = {
+//   name:"Anurag"
+// }
+
+// student.__proto__ = parent
+// student.greet();
+
+
+// //Function Prototype
+// function Greet(name){
+//   this.name = name
+// }
+
+// Greet.prototype.greet = function(){
+//   console.log(`Hello This is Greet Prototype Function ${this.name}`);
+// }
+
+// const obj1 = new Greet("Anurag")
+
+// obj1.greet()
+
+// //////////////////////Bind Apply and Call//////////////////////////
+
+// let user = {
+//   name:"Anurag",
+//   greet:function(){
+//     console.log(`Hello ${this.name}`);
+//   }
+// }
+// user.greet()
+// // let fn = user.greet.bind(user)
+// let fn = user.greet
+
+// function greet(city){
+//   console.log(this.name + " from "+city);
+// }
+
+// const user1 = {
+//   name:"Anurag"
+// }
+
+// //Call
+// greet.call(user1,"Greater Noida")
+// //Apply
+// greet.apply(user1,["Greater Noida"])
+// //Bind
+// const newFn = greet.bind(user,"Greater Noida")
+// newFn()
+
+// ////////////////////////////////////////////////////////////////////////////////////////////
+
+// //Prototype Based Language
+// let human = {
+//   study:function(){
+//     console.log(`Human Study`);
+//   }
+// }
+
+// let studybook = Object.create(human)
+// studybook.read = function(){
+//   console.log(`I read Gullivar Travels`);
+// }
+
+// studybook.read()
+// studybook.study()
+
+// /////////////////////////////////////////////////Class///////////////////////////////
+
+// /*
+// class Car{
+//   constructor(name,color){
+//     this.name = name
+//     this.color = color
+//   }
+
+//   intro(){
+//     console.log(`The Car Color is ${this.color} and Car Name is ${this.name}`);
+//   }
+// }
+
+// const car = new Car("Buggati Veyron","Red White")
+// car.intro()
+// */
+
+// class Car{
+//    static Engine= "V12"
+//   constructor(name,color){
+//     this.name = name
+//     this.color = color
+//   }
+
+//   greet(){
+//     console.log(`This car is  ${Engine}`);
+//   }
+// }
+
+// console.log(Car.Engine);
+
+//////////////////////////////////////////////////////////////////////////////////////DAY3/////////////////////////////////////////////////////////////////////////////////
+// CallBack and Fucntional Arguments
+
+function sum(a,b){
+  return a+b
 }
 
-console.log(obj.toString());
-console.log(obj.__proto__);
+function mul(a,b){
+  return a*b
+}
+
+function machine(a,b,cb){
+  return cb(a,b)
+}
+
+const ans = machine(45,56,mul)
+console.log(ans);
+
+///////////////////////Promises
+
+//Read File
+const fs = require('fs')
+function ReadFileP(file){
+  return new Promise(function(resolve,reject){
+    fs.readFile(file,'utf-8',(err,data)=>{
+      if (err) {
+        reject("Something Went Wrong")
+      }else{
+        resolve(data)
+      }
+    })
+  })
+}
 
 
-//Custom Prototype
-let parent = {
-  greet:function(){
-    console.log("Hello");
+async function main() {
+  try {
+    const value = await ReadFileP('')
+    console.log(value);
+  } catch (error) {
+    console.log(error);
   }
-} 
-
-let student = {
-  name:"Anurag"
 }
 
-student.__proto__ = parent
-student.greet();
+main()
 
+// ReadFileP('').then((data)=>{
+//   console.log(data);
+// }).catch((err)=>{
+//   console.log(err);
+// })
 
-//Function Prototype
-function Greet(name){
-  this.name = name
+////////////////////////////////////////////////////////////////////
+
+// TimeOut CallBack Hell 
+
+function setTimeOutP(s){
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      resolve()
+    },s*1000)
+  })
 }
 
-Greet.prototype.greet = function(){
-  console.log(`Hello This is Greet Prototype Function ${this.name}`);
+setTimeOutP(1).then(()=>{
+  console.log(`1 Sec Completed`);
+  setTimeOutP(3).then(()=>{
+    console.log(`3 sec Completed`);
+    setTimeOutP(5).then(()=>{
+      console.log(`5 Sec Completed`);
+    })
+  })
+})
+
+//Better way
+
+setTimeOutP(1).then(()=>{
+  console.log(`1 sec Completed`);
+  return setTimeOutP(3)
+}).then(()=>{
+  console.log(`3 sec Completed`);
+  return setTimeOutP(5)
+}).then(()=>{
+  console.log(`5 sec Completed`);
+})
+
+///////////////////////////////////////////////////////////////Async Await///////////////////////////
+
+async function main(){
+  await setTimeOutP(1)
+  console.log(`1 sec completed`);
+  await setTimeOutP(3)
+  console.log(`3 sec completed`);
+  await setTimeOutP(5)
+  console.log(`5 sec completed`);
 }
+main()
 
-const obj1 = new Greet("Anurag")
-
-obj1.greet()
-
-//////////////////////Bind Apply and Call//////////////////////////
-
-let user = {
-  name:"Anurag",
-  greet:function(){
-    console.log(`Hello ${this.name}`);
-  }
-}
-user.greet()
-// let fn = user.greet.bind(user)
-let fn = user.greet
-
-function greet(city){
-  console.log(this.name + " from "+city);
-}
-
-const user1 = {
-  name:"Anurag"
-}
-
-//Call
-greet.call(user1,"Greater Noida")
-//Apply
-greet.apply(user1,["Greater Noida"])
-//Bind
-const newFn = greet.bind(user,"Greater Noida")
-newFn()
-
-////////////////////////////////////////////////////////////////////////////////////////////
-
-//Prototype Based Language
-let human = {
-  study:function(){
-    console.log(`Human Study`);
-  }
-}
-
-let studybook = Object.create(human)
-studybook.read = function(){
-  console.log(`I read Gullivar Travels`);
-}
-
-studybook.read()
-studybook.study()
-
-/////////////////////////////////////////////////Class///////////////////////////////
-
-/*
-class Car{
-  constructor(name,color){
-    this.name = name
-    this.color = color
-  }
-
-  intro(){
-    console.log(`The Car Color is ${this.color} and Car Name is ${this.name}`);
-  }
-}
-
-const car = new Car("Buggati Veyron","Red White")
-car.intro()
-*/
-
-class Car{
-   static Engine= "V12"
-  constructor(name,color){
-    this.name = name
-    this.color = color
-  }
-
-  greet(){
-    console.log(`This car is  ${Engine}`);
-  }
-}
-
-console.log(Car.Engine);
