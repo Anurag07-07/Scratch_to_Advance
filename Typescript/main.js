7fac5e77cde7b8bc69f40376f0d62ee7701daad8
Anurag07-07 <adityanv8@gmail.com>
2026-01-04 16:01:23 +0530

    Completed Classses and Type Assertions

--- Typescript/main.js
+++ Typescript/main.js
@@ -152,8 +152,9 @@ function logMsg(message:any):void{
   console.log(message);
 }
 
-const ans1 =  sub(45,56);
-logMsg(ans1)
+
+// const ans1 =  sub(45,56);
+// logMsg(ans1)
 
 function addSum(a:number,b:number,...c:number[]):number{
   console.log(a,b);
@@ -162,24 +163,159 @@ function addSum(a:number,b:number,...c:number[]):number{
 }
 
 const ans2 = addSum(1,2,6,5,5,5,5,3,1,1)
-logMsg(ans2)
+// logMsg(ans2)
+
+
+const errMsg = (errmsg:string):never=>{
+  throw new Error(errmsg)
+}
+
+const StringorNumber =(value:any):string=>{
+  if (typeof value === 'string') {
+    return `This is String Type`
+  }
+  if (isNumber(value)) {
+    return `This is Number`
+  }
+
+  return errMsg('Please Give a Valid Input')
+}
+
+function isNumber(value:any):boolean{
+  return typeof value === 'number' ? true : false
+}
+
+StringorNumber(45)
+StringorNumber("Hello")
+StringorNumber(true)
 
 */
-var errMsg = function (errmsg) {
-    throw new Error(errmsg);
-};
-var StringorNumber = function (value) {
-    if (typeof value === 'string') {
-        return "This is String Type";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var a = "Hello";
+var b = a;
+var c = a;
+var name1 = "Anurag";
+var addorConcat = function (a, b, c) {
+    if (c === 'add') {
+        return a + b;
     }
-    if (isNumber(value)) {
-        return "This is Number";
-    }
-    return errMsg('Please Give a Valid Input');
+    return '' + a + b;
 };
-function isNumber(value) {
-    return typeof value === 'number' ? true : false;
-}
-StringorNumber(45);
-StringorNumber("Hello");
-StringorNumber(true);
+var ans = addorConcat(45, 56, 'add');
+console.log(typeof ans);
+console.log(ans);
+// Original JS code
+// const year = document.getElementById("year");
+// const thisYear = new Date().getFullYear();
+// year.setAttribute("datetime", thisYear);
+// year.textContent = thisYear;
+// const year = document.getElementById('year')! as HTMLElement
+// const thisYear:string = new Date().getFullYear().toString()
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Class and Objects
+var Coder = /** @class */ (function () {
+    function Coder(name, age, lang, music) {
+        this.name = name;
+        this.age = age;
+        this.lang = lang;
+        this.music = music;
+        this.name = name;
+        this.age = age;
+        this.music = music;
+        this.lang = lang;
+    }
+    Coder.prototype.getAge = function () {
+        console.log("".concat(this.name, " is ").concat(this.age, " year's Old"));
+    };
+    return Coder;
+}());
+var coder1 = new Coder("Anurag Raj", 21, ["Typescript,Javascript"], "Classical");
+coder1.getAge();
+var ProCoder = /** @class */ (function (_super) {
+    __extends(ProCoder, _super);
+    function ProCoder(computer, name, age, lang, music) {
+        var _this = _super.call(this, name, age, lang, music) || this;
+        _this.computer = computer;
+        _this.computer = computer;
+        return _this;
+    }
+    ProCoder.prototype.getLang = function () {
+        console.log("This Language They Follow is ".concat(this.lang));
+    };
+    return ProCoder;
+}(Coder));
+var coder2 = new ProCoder("Predator", "Anurag", 21, ["Typescript,Node,Python,C++,Java"], "Classical");
+coder2.getLang();
+var Guitarist = /** @class */ (function () {
+    function Guitarist(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+        this.name = name,
+            this.instrument = instrument;
+    }
+    Guitarist.prototype.play = function (action) {
+        return "".concat(this.name, " ").concat(action, " the ").concat(this.instrument);
+    };
+    return Guitarist;
+}());
+var guitar = new Guitarist("Anurag", "guitar");
+console.log(guitar.play("strums"));
+//////////////////////////////////Static Keyword in Class/////////////////////////////////////////////
+var Peeps = /** @class */ (function () {
+    function Peeps(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+    Peeps.getCount = function () {
+        return Peeps.count;
+    };
+    Peeps.count = 0;
+    return Peeps;
+}());
+var name2 = new Peeps("A");
+var name3 = new Peeps("B");
+var name4 = new Peeps("C");
+console.log(Peeps.getCount());
+/////////////////////////////Create A dataset of a langauge taht a developer Knows//////////////////////////////////////
+var Language = /** @class */ (function () {
+    function Language() {
+        this.dataState = [];
+    }
+    Object.defineProperty(Language.prototype, "GetLang", {
+        get: function () {
+            return this.dataState;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Language.prototype, "SetLang", {
+        set: function (value) {
+            if (Array.isArray(value) && value.every(function (el) { return typeof el === 'string'; })) {
+                this.dataState = value;
+                return;
+            }
+            throw new Error("This Data is not of Type String Array");
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Language;
+}());
+var lang = new Language();
+lang.SetLang = ["Rust", "Golang", "Typescript", "C++"];
+console.log(lang.GetLang);
--- Typescript/main.ts
+++ Typescript/main.ts
@@ -152,7 +152,7 @@ function logMsg(message:any):void{
   console.log(message);
 }
 
-*/
+
 // const ans1 =  sub(45,56);
 // logMsg(ans1)
 
@@ -188,3 +188,152 @@ function isNumber(value:any):boolean{
 StringorNumber(45)
 StringorNumber("Hello")
 StringorNumber(true)
+
+*/
+
+// function main(a:number,b:number,...c:number[]){
+//   console.log(a);
+//   console.log(b);
+//   let ans = c.reduce((prev,curr)=>prev+curr,0)
+//   console.log(ans);
+// }
+
+// main(4,5,6,1,7,9)
+
+/////////////////Type Assertions///////////////////////
+
+type One = string
+type Two = string | number
+type Three = "Hello"
+
+let a:One = "Hello"
+let b = a as Two
+let c = a as Three
+
+let name1 = <string | number>"Anurag"
+
+
+let addorConcat = (a:number,b:number,c:'add' | 'concat'):number | string=>{
+  if (c==='add') {
+    return a+b
+  }
+  return ''+a+b;
+}
+
+let ans:number = addorConcat(45,56,'add') as number 
+console.log(typeof ans);
+console.log(ans);
+
+// Original JS code
+// const year = document.getElementById("year");
+// const thisYear = new Date().getFullYear();
+// year.setAttribute("datetime", thisYear);
+// year.textContent = thisYear;
+
+// const year = document.getElementById('year')! as HTMLElement
+// const thisYear:string = new Date().getFullYear().toString()
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Class and Objects
+class Coder{
+  constructor(public readonly name:string,private age:number,protected lang:string[],public music:string){
+    this.name = name
+    this.age = age
+    this.music = music
+    this.lang = lang
+  }
+
+  public getAge(){
+    console.log(`${this.name} is ${this.age} year's Old`);
+  }
+}
+
+const coder1 = new Coder("Anurag Raj",21,["Typescript,Javascript"],"Classical")
+coder1.getAge()
+
+class ProCoder extends Coder{
+  constructor(public computer:string,name:string,age:number,lang:string[],music:string,){
+    super(name,age,lang,music);
+    this.computer = computer
+  }
+
+  public getLang(){
+    console.log(`This Language They Follow is ${this.lang}`);
+  }
+}
+
+const coder2 = new ProCoder("Predator","Anurag",21,["Typescript,Node,Python,C++,Java"],"Classical")
+coder2.getLang()
+
+
+////////////////////////////////Interface in Class/////////////////////////////////////////
+interface Musician{
+  name:string,
+  instrument:string
+  play(action:string):string
+}
+
+
+class Guitarist implements Musician{
+  constructor(public name:string,public instrument:string){
+    this.name = name,
+    this.instrument = instrument
+  }
+  play(action: string): string {
+      return `${this.name} ${action} the ${this.instrument}`
+  }
+}
+
+const guitar = new Guitarist("Anurag","guitar")
+console.log(guitar.play("strums"));
+
+//////////////////////////////////Static Keyword in Class/////////////////////////////////////////////
+
+
+class Peeps{
+  static count:number = 0
+
+  static getCount():number {
+    return Peeps.count
+  }
+
+  public id:number
+
+  constructor(public name:string){
+    this.name = name
+    this.id = ++Peeps.count
+  }
+}
+
+const name2 = new Peeps("A")
+const name3 = new Peeps("B")
+const name4 = new Peeps("C")
+
+console.log(Peeps.getCount());
+
+
+/////////////////////////////Create A dataset of a langauge taht a developer Knows//////////////////////////////////////
+//Getter and Setter in Classes 
+class Language{
+  private dataState:string[]
+  constructor(){
+    this.dataState = []
+  }
+
+  public get GetLang():string[]{
+    return this.dataState
+  }
+
+  public set SetLang(value:string[]){
+    if (Array.isArray(value) && value.every((el)=>typeof el === 'string')) {
+      this.dataState = value
+      return
+    }
+    throw new Error ("This Data is not of Type String Array")
+  }
+}
+
+const lang = new Language()
+lang.SetLang = ["Rust","Golang","Typescript","C++"]
+console.log(lang.GetLang);
