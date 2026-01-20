7dff27dadca203c3277d109ceab47083ac410367
Anurag07-07 <adityanv8@gmail.com>
2026-01-21 04:30:08 +0530

    Visible Card

---
+++ React/react-4/src/Components/Visible.tsx
@@ -0,0 +1,24 @@
+import {motion} from 'framer-motion'
+import { useState } from 'react'
+const Visible = () => {
+  const [isVisible,setIsVisible] = useState<boolean>(true)
+  return (
+    <motion.div
+     className=' w-44 h-44 bg-yellow-300'
+    variants={{
+      hidden:{opacity:0,scale:0.8},
+      visible:{opacity:1,scale:1},
+      exit:{opacity:0,scale:0.5}
+    }}
+    
+    initial={'hidden'}
+    animate={isVisible ? 'visible' : 'hidden'}
+    transition={{duration:2,ease:"easeInOut"}}
+    exit={'exit'}
+    onClick={()=>setIsVisible(!isVisible)}
+    >
+    </motion.div>
+  )
+}
+
+export default Visible
