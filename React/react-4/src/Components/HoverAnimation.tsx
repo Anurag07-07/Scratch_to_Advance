875cc14f20c9bd0151bab80aa9aad2623628dfa8
Anurag07-07 <adityanv8@gmail.com>
2026-01-21 12:58:18 +0530

    Drag Animation

--- React/react-4/src/Components/HoverAnimation.tsx
+++ React/react-4/src/Components/HoverAnimation.tsx
@@ -1,7 +1,16 @@
 import {motion} from 'framer-motion'
 const HoverAnimation = () => {
   return (
-    <motion.div whileHover={{scale:1.2,rotate:50}} transition={{type:"spring",stiffness:300}} className=" w-72 h-72 flex justify-center items-center bg-teal-300 shadow-2xl shadow-white">HoverAnimation</motion.div>
+    // <motion.div whileHover={{scale:1.2,rotate:50}} transition={{type:"spring",stiffness:300}} className=" w-72 h-72 flex justify-center items-center bg-teal-300 shadow-2xl shadow-white">HoverAnimation</motion.div>
+    // <motion.div whileTap={{scale:1.2,rotate:50}} transition={{type:"spring",stiffness:300}} className=" w-72 h-72 flex justify-center items-center bg-teal-300 shadow-2xl shadow-white">HoverAnimation</motion.div>
+    // <motion.div drag="x"   className=" w-72 h-72 flex justify-center items-center bg-teal-300 shadow-2xl shadow-white">HoverAnimation</motion.div>
+    // <motion.div drag   className=" w-72 h-72 flex justify-center items-center bg-teal-300 shadow-2xl shadow-white">HoverAnimation</motion.div>
+    <motion.div drag dragConstraints={{
+      top:-50,
+      left:-50,
+      right:50,
+      bottom:50
+    }}   className=" w-72 h-72 flex justify-center items-center bg-teal-300 shadow-2xl shadow-white">HoverAnimation</motion.div>
   )
 }
 
