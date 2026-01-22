84f77ad4dac3d9990dc17bbd35ddfc478376f9f9
Anurag07-07 <adityanv8@gmail.com>
2026-01-23 00:48:01 +0530

    Finance Tracker Completed

--- Typescript/src/FinanceTrackerX.ts
+++ Typescript/src/FinanceTrackerX.ts
@@ -1,22 +1,19 @@
-// type IncomeStream = 'salary' | 'bonus' | 'freelance'
-// type ExpenseCategory = 'houserent' | 'food' | 'travelling'
-
 type IncomeStream = 'salary' | 'bonus' | 'freelance'
-type ExpenseCategory = 'rent' | 'food' | 'travelling'
+type ExpensesCategory = 'rent' | 'food' | 'travelling'
 
 type FinanceRecord<T extends string> = Record<T,number>
 
-interface Transaction<T extends string>{
+interface Transactions<T extends string>{
   readonly id:number
-  type:'expense' | 'income'
   category:T
   amount:number
+  type:"expense" | "income"
 }
 
 class FinanceManager<I extends string,E extends string>{
-  private incomes:FinanceRecord<I>
   private expenses:FinanceRecord<E>
-  private transactions:Transaction<I | E>[] = []
+  private incomes:FinanceRecord<I>
+  private transactions:Transactions<I | E>[] = []
   
   constructor(incomes:FinanceRecord<I>,expenses:FinanceRecord<E>){
     this.expenses = expenses
@@ -32,64 +29,51 @@ class FinanceManager<I extends string,E extends string>{
     console.log(`EXPENSES`);
     for (const key in this.expenses) {
       console.log(
-        `
-        ${key} -> ${this.expenses[key as keyof typeof this.expenses]}
-        `
+       `${key} ->  ${this.expenses[key as keyof FinanceRecord<E>]}  `
       );
-    }
+    }  
   }
 
   showIncomes():void{
     console.log(`INCOMES`);
     for (const key in this.incomes) {
       console.log(
-        `
-        ${key} -> ${this.incomes[key as keyof typeof this.incomes]}
-        `
+       `${key} ->  ${this.incomes[key as keyof typeof this.incomes]}  `
       );
-    }
-  }
-
-  addIncomes<T extends I>(id:number,category:T,amount:number){
-    this.incomes[category]+=amount
-    this.transactions.push({
-      id,
-      category,
-      amount,
-      type:'income'
-    })
-  }
-
-  addExpenses<T extends E>(id:number,category:T,amount:number){
-    this.expenses[category]+=amount
-    this.transactions.push({
-      id,
-      category,
-      amount,
-      type:'expense'
-    })
-  }
-
-  getTransactions():readonly Transaction<I | E>[]{
-    return this.transactions
+    }  
   }
 
   get netbalance():number{
     return this.getTotal(this.incomes) - this.getTotal(this.expenses)
   }
+
+  addIncomes<T extends I>(id:number,category:T,amount:number){
+    this.incomes[category] += amount
+    this.transactions.push({
+      id,
+      amount,
+      category,
+      type:'income'
+    })
+  }
+
+  addExpenses<T extends E>(id:number,category:T,amount:number){
+    this.expenses[category] += amount
+    this.transactions.push({
+      id,
+      amount,
+      category,
+      type:'expense'
+    })
+  }
+
+  getTransactions():readonly Transactions<I|E>[]{
+    return this.transactions
+  }
 }
 
+const user = new FinanceManager<IncomeStream , ExpensesCategory>({salary:500000000,bonus:5000000000,freelance:5000000000},{food:100000000,rent:100000000,travelling:1000000000})
 
-const finance = new FinanceManager<IncomeStream,ExpenseCategory>(
-  { salary: 5000, bonus: 1200, freelance: 800 },
-  { food: 900, houserent: 2000, travelling: 300 },
-)
-
-
-finance.addIncomes(1,'salary',500)
-finance.addExpenses(1,'food',2000)
-
-console.log(finance.getTransactions());
-
-finance.showExpenses()
-finance.showIncomes()
+console.log(user.netbalance);
+user.showExpenses()
+user.showIncomes()
